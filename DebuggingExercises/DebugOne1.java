cf2a9dffdc278bf1336f35531a335265feb1733b
rrbaetge <rob.baetge@my.jcu.edu.au>
2017-08-02 10:29:16 +1000

    fixed bugs

--- DebuggingExercises/DebugOne1.java
+++ DebuggingExercises/DebugOne1.java
@@ -1,7 +1,8 @@
-public class DebugOne1
+public class DebugOne1{
 
    /* This program displays a greeting */
    public static void main(String[] args)
    {
-      Systemoutprintl("Hello).
+      System.out.print("Hello.");
    }
+}
--- DebuggingExercises/DebugOne3.java
+++ DebuggingExercises/DebugOne3.java
@@ -1,9 +1,7 @@
-public class DebugOne33
-{
-   public static void main(String[] args)
-   {
-      System.Out.println("Over the river");
-      system.out.println("and through the woods");
-      SysTem.0ut.println("to Grandmother's house we go");
-   }
+public class DebugOne3 {
+    public static void main(String[] args) {
+        System.out.println("Over the river");
+        System.out.println("and through the woods");
+        System.out.println("to Grandmother's house we go");
+    }
 }
